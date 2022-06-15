1c2afe4f060584de80022e8a06e2107d54ce59f0
xunne899 <tan.yixun@trentglobal.edu.sg>
2022-06-15 07:55:25 +0000

    added 08 08b

--- 08-mongo-express/index.js
+++ 08-mongo-express/index.js
@@ -1,48 +1,57 @@
 const express = require('express');
-const hbs = require ('hbs');
+const hbs = require('hbs');
 const wax = require('wax-on');
-// mongo client conatains mongo objects
+// require('mongodb') will return a Mongo object
+// the Mongo client contains many other objects (aka properties)
+// but we are only interested in the MongoClient
+// hence put `.MongoClient` at the back of the require
 const MongoClient = require('mongodb').MongoClient;
 
-// env is the environment  where operating systems stores its variables
-// when we run config  on dotenv all vars are transferred to environment
+// dotenv
+// a dotenv files also to create variables
+// when we run config on the dotenv, all variables defined in the .env file
+// are transfered to the environment
 const dotenv = require('dotenv').config();
-console.log(process.env)
+
+// process is a NodeJS object automatically available to all NodeJs programs
+// BE SURE NOT TO NAME ANY VARIABLES AS `process`
+// the process variable refers to the current NodeJS that is running
+// .env is the environment -- it is where the operating system stores its variables
+console.log(process.env);
+
 
 const app = express();
-// to explicitly tell express to use hbs as view engine instead of other view engine
-app.set('view engine','hbs')
-wax.on(hbs.handlebars);
-wax.setLayoutPath('./views/layouts')
+app.set('view engine', 'hbs'); // tell express we are using hbs as the view engine
+wax.on(hbs.handlebars);  // setup template inheritance
+wax.setLayoutPath('./views/layouts');
 
 const MONGO_URI = process.env.MONGO_URI;
 
-async function main(){
-    // 1connection string //  2options object
-    const client = await MongoClient.connect(MONGO_URI,{
-        'useUnifiedTopology': true
+async function main() {
+    // need to the Mongo Database using the MongoClient
+    // the MongoClient.connect function takes in two arguments:
+    // 1) the connection string
+    // 2) an options object
+    const client = await MongoClient.connect(MONGO_URI, {
+        "useUnifiedTopology": true // there were different versions of Mongo
+                                  // when this is true we don't have to care about those versions
     })
 
+    const db = client.db('sample_airbnb');
 
-// connect to respective mongo database    
-const db = client.db('sample_airbnb')
-// go to the respective route
-app.get('/test', async function(req,res){
-    // use toArray() to convert results to an array of js objects
- let data = await db.collection('listingsAndReviews').find({}).limit(10).toArray();
- res.send(data)
-})
+    app.get('/test', async function(req,res) {
+        // we use the .toArray() to convert the results to an array of JavaScript objects
+        let data = await db.collection('listingsAndReviews').find({}).limit(10).toArray();
+        res.send(data);
+    });
 
-app.get('/', function(req,res){
-    res.render('hello.hbs')
-})
-
-
+    app.get('/', function (req, res) {
+        res.render('hello.hbs')
+    })
 }
-
-
 main();
-app.listen(3000, function(){
+
+
+app.listen(3000, function () {
     console.log("hello world");
 })
-
--- 08b-mongo-crud/index.js
+++ 08b-mongo-crud/index.js
@@ -1,48 +1,89 @@
 const express = require('express');
-const hbs = require ('hbs');
+const hbs = require('hbs');
 const wax = require('wax-on');
-// mongo client conatains mongo objects
-const MongoClient = require('mongodb').MongoClient;
+// we need the ./ in front our own custom modules
+// because without it, NodeJS will assume that
+// we are requiring from the node_modules folder
+// const {connect} = require('./MongoUtil');
+//./ to indicate from current folder
+const MongoUtil = require('./MongoUtil')
 
-// env is the environment  where operating systems stores its variables
-// when we run config  on dotenv all vars are transferred to environment
+
+// dotenv
+// a dotenv files also to create variables
+// when we run config on the dotenv, all variables defined in the .env file
+// are transfered to the environment
 const dotenv = require('dotenv').config();
-console.log(process.env)
+
+// process is a NodeJS object automatically available to all NodeJs programs
+// BE SURE NOT TO NAME ANY VARIABLES AS `process`
+// the process variable refers to the current NodeJS that is running
+// .env is the environment -- it is where the operating system stores its variables
+// console.log(process.env);
+
 
 const app = express();
-// to explicitly tell express to use hbs as view engine instead of other view engine
-app.set('view engine','hbs')
-wax.on(hbs.handlebars);
-wax.setLayoutPath('./views/layouts')
+app.set('view engine', 'hbs'); // tell express we are using hbs as the view engine
+wax.on(hbs.handlebars);  // setup template inheritance
+wax.setLayoutPath('./views/layouts');
+
+// so that submitted form content will be inside req.body
+app.use(express.urlencoded({
+    extended: false
+}));
 
 const MONGO_URI = process.env.MONGO_URI;
 
-async function main(){
-    // 1connection string //  2options object
-    const client = await MongoClient.connect(MONGO_URI,{
-        'useUnifiedTopology': true
+
+async function main() {
+  
+    const db = await MongoUtil.connect(MONGO_URI, "tgc18_cico");
+    app.get('/test', async function(req,res) {
+        // we use the .toArray() to convert the results to an array of JavaScript objects
+        let data = await db.collection('listingsAndReviews').find({}).limit(10).toArray();
+        res.send(data);
+    });
+
+    app.get('/', async function (req, res) {
+        const allFoodRecords = await db.collection('food_records')
+                                 .find({}) 
+                                 .toArray();
+
+        res.render('all-food.hbs',{
+            'allFood': allFoodRecords
+        })
     })
 
+    // one route to display the form
+    app.get('/add-food', function(req,res){
+        res.render('add-food.hbs');
+    })
 
-// connect to respective mongo database    
-const db = client.db('sample_airbnb')
-// go to the respective route
-app.get('/test', async function(req,res){
-    // use toArray() to convert results to an array of js objects
- let data = await db.collection('listingsAndReviews').find({}).limit(10).toArray();
- res.send(data)
-})
+    // one route to process the form
+    app.post('/add-food', async function(req,res){
+        let foodRecordName = req.body.foodRecordName;
+        let calories = req.body.calories;
+        let tags = [];
+        if (Array.isArray(req.body.tags)) {
+            tags = req.body.tags;
+        } else if (req.body.tags) {
+            tags = [ req.body.tags ];
+        }
 
-app.get('/', function(req,res){
-    res.render('hello.hbs')
-})
+        let foodDocument = {
+            'food': foodRecordName,
+            'calories': calories,
+            'tags': tags
+        };
 
+        await db.collection('food_records').insertOne(foodDocument);
+        res.redirect("/");
 
+    })
 }
-
-
 main();
-app.listen(3000, function(){
+
+
+app.listen(3000, function () {
     console.log("hello world");
 })
-
