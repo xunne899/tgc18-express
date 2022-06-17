a57446f67578d35614d1457b2c64b8fa61327a5f
xunne899 <tan.yixun@trentglobal.edu.sg>
2022-06-17 16:34:49 +0000

    add new lab08 handson c

--- lab-08-mongo-handson_C_tryout/index.js
+++ lab-08-mongo-handson_C_tryout/index.js
@@ -80,6 +80,59 @@ app.post('/create', async function(req,res){
     <li>Name: {{this.name}}</li>
     <li>Email: {{this.email}}</li>
 </ul> */
+app.get('/:id/edit',async (req,res)=>{
+    let faults = await db.collection(FAULTS).findOne({
+        '_id':ObjectId(req.params.id)
+    })
+
+    res.render('edit-faults',{
+       faults
+    })
+})
+
+app.post('/:id/edit',async(req,res)=>{
+ let id =req.params.id
+ let tags = checboxValues(req.body.tags)
+ let newinfo = {
+    'title': req.body.title,
+    'location':req.body.location.split(','),
+    'tags': tags,
+    'block':req.body.block,
+    'name':req.body.name,
+    'email':req.body.email
+}
+
+await db.collection(FAULTS).updateOne({
+    _id:ObjectId(id)
+
+},{
+    '$set':newinfo
+})
+
+res.redirect('/')
+})
+
+
+
+
+app.get('/delete-faults/:id', async function(req,res){
+    let id = req.params.id;
+    let  faults = await db.collection(FAULTS).findOne({
+        '_id': ObjectId(id)
+    })
+    res.render('delete-faults',{
+        faults
+    })
+})
+
+app.post('/delete-faults/:id',async function(req,res){
+    let id = req.params.id;
+    await db.collection(FAULTS).deleteOne({
+    '_id':ObjectId(id)
+})
+res.redirect('/')
+})
+
 
 app.listen(3001, function(){
     console.log("Server has started")
